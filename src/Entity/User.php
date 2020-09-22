47c3e99ef917ed92e31484aeae4614cad609f107
Lucas GRARD <lucas.grard@outlook.fr>
2020-09-22 17:13:41 +0200

    save

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -190,7 +190,6 @@ class User implements UserInterface
         return $this;
     }
 
-<<<<<<< HEAD
     public function getConfirmMail(): ?ConfirmMail
     {
         return $this->ConfirmMail;
@@ -199,14 +198,6 @@ class User implements UserInterface
     public function setConfirmMail(?ConfirmMail $ConfirmMail): self
     {
         $this->ConfirmMail = $ConfirmMail;
-=======
-    /**
-     * @return Collection|Participant[]
-     */
-    public function getParticipants(): Collection
-    {
-        return $this->participants;
-    }
 
     public function addParticipant(Participant $participant): self
     {
@@ -214,51 +205,6 @@ class User implements UserInterface
             $this->participants[] = $participant;
             $participant->setUser($this);
         }
-
-        return $this;
-    }
-
-    public function removeParticipant(Participant $participant): self
-    {
-        if ($this->participants->contains($participant)) {
-            $this->participants->removeElement($participant);
-            // set the owning side to null (unless already changed)
-            if ($participant->getUser() === $this) {
-                $participant->setUser(null);
-            }
-        }
-
-        return $this;
-    }
-
-    /**
-     * @return Collection|Message[]
-     */
-    public function getMessages(): Collection
-    {
-        return $this->messages;
-    }
-
-    public function addMessage(Message $message): self
-    {
-        if (!$this->messages->contains($message)) {
-            $this->messages[] = $message;
-            $message->setUser($this);
-        }
-
-        return $this;
-    }
-
-    public function removeMessage(Message $message): self
-    {
-        if ($this->messages->contains($message)) {
-            $this->messages->removeElement($message);
-            // set the owning side to null (unless already changed)
-            if ($message->getUser() === $this) {
-                $message->setUser(null);
-            }
-        }
->>>>>>> master
 
         return $this;
     }
